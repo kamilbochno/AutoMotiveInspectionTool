189dca70cfe50c1900e93c0aae1f2e7cd5b18979
Kamil Bochno <kamilas12345@gmail.com>
2021-07-02 14:24:18 +0200

    Change style to bootstrap

--- src/Components/Loginform/Loginform.js
+++ src/Components/Loginform/Loginform.js
@@ -1,28 +1,30 @@
 import React from 'react';
-import Button from '@material-ui/core/Button';
+import { Form, Button } from 'react-bootstrap';
 
 import styles from './Loginform.css';
 
 function Login() {
     return (
     <div className="Login">
+        <h3 className="sign-in">Sign in</h3>
         <span className="close-btn"><a href="/">x</a></span>
-        <form className="login-form">
-         <h3>Sign in</h3>
-            <label>
-                Username:
-                <input type="text" name="username" placeholder="Enter username" />
-               
-            </label>
-            <label>
-                Password:
-                <input type="password" name="password" placeholder="Enter password" />
-            </label>
-            <Button style={{ marginTop: "20px"}} href="/" variant="contained" color="primary">
-               Log in
+        <Form className="login-form">
+            <Form.Group controlId="username">
+                <Form.Label>Username: </Form.Label>
+                <Form.Control type="text" placeholder="Enter username" />
+                
+            </Form.Group>
+
+            <Form.Group controlId="password">
+                <Form.Label>Password: </Form.Label>
+                <Form.Control type="password" placeholder="Enter password" />
+            </Form.Group>
+            
+            <Button variant="primary" type="submit">
+                Submit
             </Button>
             <p>Do not have an account? Register <a href="/Register">Here</a></p>
-        </form>
+</Form>
     </div>
     );
 }
