14adb0bc3da597ed81aa086f4bf1c6634f9fd16f
Kamil Bochno <kamilas12345@gmail.com>
2021-07-07 09:37:23 +0200

    Add p and carousel to homepage

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -3,6 +3,11 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 import Button from '@material-ui/core/Button';
 import Container from '@material-ui/core/Container';
+import Slide1 from './slide1_1.jpg';
+import Slide2 from './slide2_2.jpg';
+import Slide3 from './slide3_3.jpg';
+
+import { Carousel } from 'react-bootstrap';
 
 
 import styles from './Home.css';
@@ -40,6 +45,33 @@ function Home() {
                 <Container className="Welcome">
                     <h1>Welcome to AutoMotiveInspectionTool</h1>
                     <p>For use the app please use the navigation menu</p>
+                    <p>This app can be used for adding vehicles to list and edit it's informations.</p>
+                    <Carousel className="carousel">
+                        <Carousel.Item>
+                            <img
+                                className="d-block w-100"
+                                src={Slide1}
+                                alt="First slide"
+                            />    
+                        </Carousel.Item>
+                        <Carousel.Item>
+                            <img
+                                className="d-block w-100"
+                                src={Slide2}
+                                alt="Second slide"
+                            />    
+                        </Carousel.Item>
+                        <Carousel.Item>
+                            <img
+                                className="d-block w-100"
+                                src={Slide3}
+                                alt="Third slide"
+                            />    
+                        </Carousel.Item>
+                    </Carousel>
+                    
+
+                       
                 </Container>
             </div>
         </div>
