9c3cc786dcf907276bf36773ee2f599751ad9cd8
Kamil Bochno <kamilas12345@gmail.com>
2021-07-19 20:56:28 +0200

    Stylize modal with vehicle info

--- src/Components/Vehicles/infomodal.js
+++ src/Components/Vehicles/infomodal.js
@@ -13,18 +13,23 @@ const Infomodal = ({ handleClose, showinfo,info }) => {
 
 
 <div className={showHideClassName}>
-<section className="modal-main">
+<section className="info-modal-main">
   {info && (
     <div>
-<h1>{info.name}</h1>
-<h3>{info.model}</h3>
-<p>{info.year}</p>
-<p>{info.color}</p>
-<p>{info.price}</p>
-<p>{info.car_body}</p>
-</div>
+        <h1>Vehicle info</h1>
+        <h2>Brand: {info.name}</h2>
+        <h3>Model: {info.model}</h3>
+        <p>Year of production: {info.year}</p>
+        <p>Color: {info.color}</p>
+        <p>Price: {info.price} $</p>
+        <p>Car body style: {info.car_body}</p>
+        <p>Car fuel type: {info.car_fuel}</p>
+        <p>Car engine capacity: {info.car_capacity}</p>
+        <p>Engine power: {info.HP}</p>
+
+    </div>
   )}
-  <Button className="M-close" onClick={handleClose}>
+  <Button size="lg" className="M-close" onClick={handleClose}>
     Close
   </Button>
 </section>
